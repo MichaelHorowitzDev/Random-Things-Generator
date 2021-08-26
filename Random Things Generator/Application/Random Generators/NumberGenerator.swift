a721e9ed8a3cd3eab058f5a2dd18be4ef2de24a9
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-08-26 17:24:16 -0400

    added basic interface

--- Random Things Generator/Application/Random Generators/NumberGenerator.swift
+++ Random Things Generator/Application/Random Generators/NumberGenerator.swift
@@ -8,14 +8,54 @@
 import SwiftUI
 
 struct NumberGenerator: View {
+  @State private var firstNumber = ""
+  @State private var secondNumber = ""
+  @FocusState private var isFocused: Bool
     var body: some View {
       ZStack {
-        Color.blue.ignoresSafeArea()
+        Color.blue.ignoresSafeArea(.all, edges: [.horizontal, .bottom])
         VStack {
-          
+          HStack(spacing: 20) {
+            NumberEntry(placeholder: "First Number", string: $firstNumber, isFocused: $isFocused)
+            NumberEntry(placeholder: "Second Number", string: $secondNumber, isFocused: $isFocused)
+          }
+          .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+              Spacer()
+              Button("Done") {
+                isFocused = false
+              }
+            }
+          }
+          .padding()
         }
+        VStack {
+          Spacer()
+          RandomizeButton(buttonTitle: "Randomize") {
+            print("pressed")
+          }
+        }
+        .navigationTitle("Number")
+        .navigationBarTitleDisplayMode(.inline)
       }
     }
+}
+
+private struct NumberEntry: View {
+  let placeholder: String
+  @Binding var string: String
+  private var isFocused: FocusState<Bool>.Binding
+  init(placeholder: String, string: Binding<String>, isFocused: FocusState<Bool>.Binding) {
+    self.placeholder = placeholder
+    self._string = string
+    self.isFocused = isFocused
+  }
+  var body: some View {
+    TextField(placeholder, text: $string)
+      .textFieldStyle(.roundedBorder)
+      .keyboardType(.numberPad)
+      .focused(isFocused)
+  }
 }
 
 struct NumberGenerator_Previews: PreviewProvider {
