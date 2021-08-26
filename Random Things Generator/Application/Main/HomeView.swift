cb3df9029152bc43dacddbbab1348ae7f0b26867
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-08-26 01:41:59 -0400

    changed indent to 2 spaces

--- Random Things Generator/Application/Main/HomeView.swift
+++ Random Things Generator/Application/Main/HomeView.swift
@@ -8,45 +8,46 @@
 import SwiftUI
 
 struct HomeView: View {
-    let types = ["Number", "Word", "Card", "Coin", "Date", "Password"]
-    var body: some View {
-        NavigationView {
-            VStack {
-                ForEach(0..<(types.count+1)/2) { num in
-                    HStack(spacing: 20) {
-                        HomeViewItem(item: types[num])
-                        if num+1 < types.count {
-                            HomeViewItem(item: types[num+1])
-                        }
-                    }
-                    .padding([.leading, .trailing])
-                }
-                Spacer()
+  let types = ["Number", "Word", "Card", "Coin", "Date", "Password"]
+  var body: some View {
+    NavigationView {
+      ScrollView {
+        VStack {
+          ForEach(0..<(types.count+1)/2) { num in
+            HStack(spacing: 20) {
+              HomeViewItem(item: types[num])
+              if num+1 < types.count {
+                HomeViewItem(item: types[num+1])
+              }
             }
-            .navigationBarTitle("Random")
+            .padding([.leading, .trailing])
+          }
         }
+      }
+      .navigationBarTitle("Random")
     }
+  }
 }
 
 private struct HomeViewItem: View {
-    let item: String
-    var color: Color = .blue
-    var body: some View {
-        RoundedRectangle(cornerRadius: 15)
-            .fill(color)
-            .frame(height: 100)
-            .overlay(
-                Text(item)
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .foregroundColor(color.isLight ? .black : .white)
-                
-            )
-    }
+  let item: String
+  var color: Color = .blue
+  var body: some View {
+    RoundedRectangle(cornerRadius: 15)
+      .fill(color)
+      .frame(height: 100)
+      .overlay(
+        Text(item)
+          .font(.title)
+          .fontWeight(.semibold)
+          .foregroundColor(color.isLight ? .black : .white)
+        
+      )
+  }
 }
 
 struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-    }
+  static var previews: some View {
+    HomeView()
+  }
 }
