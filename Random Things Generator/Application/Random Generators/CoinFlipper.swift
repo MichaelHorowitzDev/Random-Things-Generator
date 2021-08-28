09c712b44ce8d92c126b5f3b29336a8511e929b5
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-08-28 00:41:48 -0400

    added half dollar images

---
+++ Random Things Generator/Application/Random Generators/CoinFlipper.swift
@@ -0,0 +1,20 @@
+//
+//  CoinFlipper.swift
+//  CoinFlipper
+//
+//  Created by Michael Horowitz on 8/28/21.
+//
+
+import SwiftUI
+
+struct CoinFlipper: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct CoinFlipper_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinFlipper()
+    }
+}
