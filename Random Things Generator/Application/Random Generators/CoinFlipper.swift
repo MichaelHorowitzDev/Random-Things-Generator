da6cfbd00547b47752f76e8cb545ce548a6619a0
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-08-28 03:01:03 -0400

    coin flipper works

--- Random Things Generator/Application/Random Generators/CoinFlipper.swift
+++ Random Things Generator/Application/Random Generators/CoinFlipper.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct CoinFlipper: View {
+  @State private var heads = true
+  @EnvironmentObject var preferences: UserPreferences
+  @State private var fadeOut = false
+  @State private var animationAmount: CGFloat = 1
     var body: some View {
-        Text("Hello, World!")
+      RandomGeneratorView {
+        VStack {
+          Text(heads ? "Heads" : "Tails")
+            .foregroundColor(preferences.textColor)
+            .font(.system(size: 60, weight: .medium, design: .rounded))
+            .minimumScaleFactor(0.3)
+            .padding(.top, 25)
+            .padding(.horizontal, 20)
+            .scaleEffect(animationAmount)
+          Spacer()
+        }
+        Image(heads ? Coins.halfDollarObverse : Coins.halfDollarReverse)
+          .resizable()
+          .scaledToFit()
+          .padding(20)
+          .scaleEffect(animationAmount)
+      }
+      .randomButtonTitle("Flip")
+      .onRandomPressed {
+        animationAmount = 0.97
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) {
+          withAnimation(.linear(duration: 0.05)) {
+            animationAmount = 1
+            heads = Bool.random()
+          }
+          
+        }
+      }
+      .navigationTitle("Coin")
     }
+}
+private struct Coins {
+  static var halfDollarObverse = "half_dollar_obverse"
+  static var halfDollarReverse = "half_dollar_reverse"
 }
 
 struct CoinFlipper_Previews: PreviewProvider {
