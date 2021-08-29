64a477f9e1e4c4fc1aa0fef87e3c0f4e5d0dbc5c
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-08-29 02:16:07 -0400

    added random card view

--- Random Things Generator/Application/Random Generators/CardRandomizer.swift
+++ Random Things Generator/Application/Random Generators/CardRandomizer.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct CardRandomizer: View {
+  @State private var currentCard = ["RED_BACK"]
+  @State private var animationAmount: CGFloat = 1
+  @EnvironmentObject var preferences: UserPreferences
+  @State private var cardCount = 1
+  let columns: [GridItem] = [GridItem(.flexible())]
     var body: some View {
-      RandomGeneratorView {
-        Image("RED_BACK")
+      ZStack {
+        VStack {
+          Text("Card Count: \(cardCount)")
+            .font(.system(.title, design: .rounded))
+            .fontWeight(.medium)
+          Stepper("Card Count", value: $cardCount, in: 1...6)
+            .labelsHidden()
+            .padding()
+          Spacer()
+        }
+        .zIndex(1)
+        .foregroundColor(preferences.textColor)
+        .padding(.top, 40)
+        RandomGeneratorView {
+          Image(currentCard)
+            .scaleEffect(animationAmount)
+        }
+        .onRandomTouchDown {
+          animationAmount = 0.97
+        }
+        .onRandomTouchUp {
+          animationAmount = 1
+        }
+        .onRandomPressed {
+          let value = "23456789TJQK".randomElement()!
+          let suit = "CHSD".randomElement()!
+          currentCard = String(value).appending(String(suit))
+        }
       }
     }
 }
