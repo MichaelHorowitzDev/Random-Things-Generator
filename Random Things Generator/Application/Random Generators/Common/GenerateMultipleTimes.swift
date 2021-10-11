7913b9419e155c993d60a0997f4519267d04ef1a
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-10-11 17:38:36 -0400

    added generate multiple views

--- Random Things Generator/Application/Random Generators/Common/GenerateMultipleTimes.swift
+++ Random Things Generator/Application/Random Generators/Common/GenerateMultipleTimes.swift
@@ -19,6 +19,8 @@ struct GenerateMultipleTimes: View {
   @State private var results = [String]()
   @State private var sheetPresented = false
   @State private var numberOfTimes = ""
+  @State private var generateStatistics = false
+  @State private var shareItem: ShareItem?
   @EnvironmentObject var preferences: UserPreferences
     var body: some View {
 //      NavigationView {
@@ -47,7 +49,6 @@ struct GenerateMultipleTimes: View {
               Text("Generate")
                 .foregroundColor(.white)
                 .padding(5)
-//                .padding(.vertical, )
                 .background(.blue)
                 .cornerRadius(10)
             }
@@ -55,8 +56,8 @@ struct GenerateMultipleTimes: View {
           .padding(.horizontal)
           List(0..<results.count, id: \.self) { num in
             HStack {
-              Text("\(num+1). ")
-                .font(.title)
+//              Text("\(num+1). ")
+//                .font(.title)
               Spacer()
               if formatValue != nil {
                 formatValue!(results[num])
@@ -67,65 +68,49 @@ struct GenerateMultipleTimes: View {
             }
             .padding(.vertical, 5)
           }
+          NavigationLink(isActive: $generateStatistics) {
+            GenerateMultipleTimesStatistics(results: results)
+          } label: {
+            EmptyView()
+          }
         }
+        .sheet(item: $shareItem, onDismiss: {
+          shareItem = nil
+        }, content: { item in
+          ShareSheet(activityItems: [item.url])
+        })
         .navigationTitle("Multiple Generator")
         .toolbar {
           ToolbarItem(placement: .navigationBarTrailing) {
             Menu {
-              NavigationLink {
-                GenerateMultipleTimesStatistics(results: results)
-              } label: {
-                Text("Results Statistics")
+              if !results.isEmpty {
+                Button {
+                  generateStatistics = true
+                } label: {
+                  HStack {
+                    Text("Statistics")
+                    Image(systemName: "chart.bar")
+                  }
+                }
+                Button {
+                  let saver = CSVSave(namePrefix: "generate_multiple_times", rows: [results])
+                  let url = saver.save()
+                  shareItem = ShareItem(url: url)
+                } label: {
+                  HStack {
+                    Text("Save")
+                    Image(systemName: "square.and.arrow.down")
+                  }
+                }
               }
-
             } label: {
               Image(systemName: "ellipsis")
             }
           }
-//          ToolbarItem(placement: .navigationBarTrailing) {
-//            CSVSaver(namePrefix: "multiple_generator", rows: [results])
-//          }
         }
-        
-//      }
-//      if showsAlert {
-//        TextFieldAlert(show: $showsAlert, title: "Generate multiple times", message: "Generate a random item multiple times", placeholder: "Number of times") { string in
-//          if let numberOfTimes = Int(string ?? "") {
-//            if numberOfTimes > 0 {
-//              let results = (1...numberOfTimes).map { _ in
-//                function()
-//              }
-//              self.results = ResultsItem(results: results)
-//              print(results)
-//              print(results.count)
-//            }
-//          }
-//        }
-//      }
-//      Button {
-//        showsAlert = true
-//      } label: {
-//        Text("Generate multiple times")
-//      }
-//      .sheet(item: $results) { results in
-//        NavigationView {
-//          #warning("sheet dismisses itself")
-//          List(0..<results.results.count, id: \.self) {_ in
-//            Text("dsad")
-////            Text(results.results[$0])
-//          }
-//          .navigationTitle("Generated")
-//        }
-//      }
     }
 }
 private struct ResultsItem: Identifiable {
   let id = UUID()
   let results: [String]
 }
-
-//struct GenerateMultipleTimes_Previews: PreviewProvider {
-//    static var previews: some View {
-//        GenerateMultipleTimes()
-//    }
-//}
---
+++ Random Things Generator/Application/Random Generators/Common/GenerateMultipleTimesStatistics.swift
@@ -0,0 +1,77 @@
+//
+//  GenerateMultipleTimesStatistics.swift
+//  Random Things Generator
+//
+//  Created by Michael Horowitz on 10/10/21.
+//
+
+import SwiftUI
+
+struct GenerateMultipleTimesStatistics: View {
+  let results: [String]
+  @State private var sortedItems: [Common<String>]
+  @State private var sortedBy: String = "Most Common"
+  let sorts = ["Most Common", "Least Common"]
+  init(results: [String]) {
+    self.results = results
+    self._sortedItems = State(initialValue: results.mostCommon())
+  }
+    var body: some View {
+      List {
+        Section {
+          ForEach(0..<sortedItems.count, id: \.self) { num in
+            HStack {
+              Text("\(num+1). ")
+                .font(.title)
+              Spacer()
+              Text(sortedItems[num].item)
+                .font(.title)
+                .bold()
+                .padding(.trailing)
+              Text("\(sortedItems[num].num)")
+                .font(.title2)
+            }
+          }
+        } header: {
+          HStack {
+            Text("Sorted By")
+            Spacer()
+            Picker("Time Range", selection: $sortedBy) {
+              ForEach(sorts, id: \.self) {
+                Text($0)
+              }
+            }
+            .pickerStyle(.menu)
+            .textCase(nil)
+            .padding(.horizontal, 5)
+            .padding(.vertical, 3)
+            .overlay(
+              RoundedRectangle(cornerRadius: 10)
+                .stroke(.blue, lineWidth: 1)
+            )
+            .padding(.bottom, 3)
+          }
+          .onChange(of: sortedBy) { newValue in
+            print(sortedBy)
+            print(newValue)
+            switch newValue {
+            case "Most Common":
+              sortedItems = results.mostCommon()
+            case "Least Common":
+              sortedItems = results.leastCommon()
+            default:
+              break
+            }
+          }
+        }
+      }
+      .navigationTitle("Statistics")
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          CSVSaver(namePrefix: "generate_multiple_statistics", rows: (sortedItems.map { [$0.item, $0.num.description] })) {
+            Image(systemName: "square.and.arrow.up")
+          }
+        }
+      }
+    }
+}
