8ee9353cc1ae5363d9c12c374aca8299a97ba794
MichaelHorowitzDev <michaelhorowitzdev@gmail.com>
2021-10-24 00:41:20 -0400

    added share sheet and rate ability

--- Random Things Generator/Application/SettingsMenu/SettingsMenu.swift
+++ Random Things Generator/Application/SettingsMenu/SettingsMenu.swift
@@ -14,6 +14,7 @@ struct SettingsMenu: View {
   @State private var showsMail = false
   @State private var result: Result<MFMailComposeResult, Error>? = nil
   @State private var emailCopied = false
+  @State private var showsShareSheet = false
     var body: some View {
       NavigationView {
         List {
@@ -48,12 +49,18 @@ struct SettingsMenu: View {
             }
           }
           Section("Support") {
-            Button("Send Email") {
+            Button {
               if MFMailComposeViewController.canSendMail() {
                 showsMail = true
               } else {
                 UIPasteboard.general.string = "michaelhorowitzdev@gmail.com"
                 emailCopied = true
+              }
+            } label: {
+              HStack {
+                Text("Send Email")
+                Spacer()
+                Image(systemName: "paperplane")
               }
             }
             .alert("Email Copied", isPresented: $emailCopied, actions: {
@@ -69,6 +76,29 @@ struct SettingsMenu: View {
               .setToRecipients(["michaelhorowitzdev@gmail.com"])
               .setMessageBody("<p><br><br><br><br>Version \(version)</p>", isHTML: true)
               .setSubject("Random Things Generator")
+            }
+            if let url = URL(string: "https://itunes.apple.com/us/app/random-things-generator/id1460779766"), UIApplication.shared.canOpenURL(url) {
+              Button {
+                UIApplication.shared.open(url.appendingPathComponent("?action=write-review"))
+              } label: {
+                HStack {
+                  Text("Rate App")
+                  Spacer()
+                  Image(systemName: "star")
+                }
+              }
+              Button {
+                showsShareSheet = true
+              } label: {
+                HStack {
+                  Text("Share")
+                  Spacer()
+                  Image(systemName: "square.and.arrow.up")
+                }
+              }
+              .sheet(isPresented: $showsShareSheet) {
+                ShareSheet(activityItems: ["Check out the Random Things Generator App on the App Store!", url])
+              }
             }
           }
         }
